89877b73b886cffc37a25de548edb38552ce7394
sam rey <samrey2018@gmail.com>
2020-06-04 14:35:26 +0000

    Changes complete for the lab.  And working

--- jpm_module_1/client.py
+++ jpm_module_1/client.py
@@ -17,7 +17,8 @@
 #  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 #  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 #  DEALINGS IN THE SOFTWARE.
-
+import unittest
+from client import *
 import urllib2
 import time
 import json
